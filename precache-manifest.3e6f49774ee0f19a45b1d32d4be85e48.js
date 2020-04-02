76df979f33ff0589f4318356b01c4af7811db981
Luiz Henrique <luizhdinizf@gmai.com>
2020-04-01 22:16:41 -0300

    Updates

---
+++ precache-manifest.3e6f49774ee0f19a45b1d32d4be85e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa3bfc98344e93ddcce34b2ae0b6a48",
+    "url": "/reactjs-todolist/index.html"
+  },
+  {
+    "revision": "a1a75fede4656d7604fe",
+    "url": "/reactjs-todolist/static/css/main.f7c177c4.chunk.css"
+  },
+  {
+    "revision": "e0a8132b8d64c1d02cd0",
+    "url": "/reactjs-todolist/static/js/2.f3ee93ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs-todolist/static/js/2.f3ee93ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a75fede4656d7604fe",
+    "url": "/reactjs-todolist/static/js/main.0e295eca.chunk.js"
+  },
+  {
+    "revision": "064611f3e7a2b4668911",
+    "url": "/reactjs-todolist/static/js/runtime-main.974e683c.js"
+  }
+]);

a18c094588ee06781a20bf2b2c20720089746be7
Luiz Henrique <luizhdinizf@gmai.com>
2020-04-01 22:10:07 -0300

    creating docs folder for githubpages

---
+++ docs/precache-manifest.87a7e96c7e722979c7c0c5c618baeb8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a715a44cd4ebda2eb2f2d3662085c81f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a75fede4656d7604fe",
+    "url": "/static/css/main.f7c177c4.chunk.css"
+  },
+  {
+    "revision": "e0a8132b8d64c1d02cd0",
+    "url": "/static/js/2.f3ee93ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f3ee93ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a75fede4656d7604fe",
+    "url": "/static/js/main.0e295eca.chunk.js"
+  },
+  {
+    "revision": "7b2d5f1aa321b80deedc",
+    "url": "/static/js/runtime-main.2dd38f42.js"
+  }
+]);

b3b63e4db54bb427fe47d47b60f6f2c3c3a01f53
Luiz Henrique <luizhdinizf@gmai.com>
2020-04-01 22:20:58 -0300

    creating docs folder for githubpages

--- Frontend/todolist/src/routes.js
+++ Frontend/todolist/src/routes.js
@@ -8,7 +8,7 @@ export default function Routes(){
     return(        
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={TasksList}/>
+                <Route path="/tasks" exact component={TasksList}/>
                 
 
             </Switch>
